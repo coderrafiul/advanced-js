66f1b7fb82c57736754d59153c6c8a22d0602b9c
coderrafiul <rafiul.rzb@gmail.com>
2020-03-06 06:30:22 +0600

    truthy or falsy

---
+++ true-false.js
@@ -0,0 +1,32 @@
+// Falsy
+0
+""
+null
+undefined
+NaN
+false
+
+// Trurhy
+"0"
+" "
+"false"
+[]
+{}
+
+
+
+
+// const age= 0;
+// const name= "";
+
+let name= NaN;
+
+console.log(name);
+
+if(name){
+    console.log("condition is true")
+}
+
+else{
+    console.log("condition is false")
+}
