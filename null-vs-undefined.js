2fe51e52f10abb82183e988f3b4b8282b426787d
coderrafiul <rafiul.rzb@gmail.com>
2020-03-06 07:11:27 +0600

    null-vs- undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+let name;
+console.log(name);
+
+function add (num1, num2){
+    const output= num1 + num2;
+    console.log(output);
+}
+
+const result= add (15, 17);
+console.log(result);
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result= add(15)
+console.log(result);
+
+
+const loverboy = {id:25, name: "sakib khan"};
+console.log(loverboy.gf)
+
+
+const fun= undefined;
+console.log(fun);
+
+
+const ages=[12, 15, 21];
+console.log(ages[11]);
